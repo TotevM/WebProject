dced89e3c82bca8738f197a0a887ad9a959783f8
TotevM <mtotev53@gmail.com>
2024-10-27 23:27:12 +0200

    Added Controllers

---
+++ FitnessApp.Web/Controllers/DietController.cs
@@ -0,0 +1,23 @@
+﻿using FitnessApp.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitnessApp.Web.Controllers
+{
+	public class DietController : Controller
+	{
+		private readonly ILogger<DietController> logger;
+		private readonly UserManager<ApplicationUser> user;
+
+		public DietController(ILogger<DietController> _logger, UserManager<ApplicationUser> _user)
+		{
+			logger = _logger;
+			user = _user;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ FitnessApp.Web/Controllers/ProgressController.cs
@@ -0,0 +1,24 @@
+﻿using FitnessApp.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitnessApp.Web.Controllers
+{
+	public class ProgressController : Controller
+	{
+		private readonly ILogger<ProgressController> logger;
+		private readonly UserManager<ApplicationUser> user;
+
+		public ProgressController(ILogger<ProgressController> _logger, UserManager<ApplicationUser> _user)
+		{
+			logger = _logger;
+			user = _user;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+	}
+}
