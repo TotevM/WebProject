e111c16094e36c218eceaf0bf679e4fcb3a293e4
TotevM <mtotev53@gmail.com>
2024-11-12 10:48:20 +0200

    Add Recipe To Diet Logic Implemented

---
+++ FitnessApp.ViewModels/RecipeModels/AddRecipeToDietViewModel.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+public class AddRecipeToDietViewModel
+{
+	public Guid RecipeId { get; set; }
+	public IEnumerable<SelectListItem>? Diets { get; set; } // Dropdown list of existing diets
+	public Guid SelectedDietId { get; set; } // To store the selected diet
+}
--- FitnessApp.Web/Controllers/DietController.cs
+++ FitnessApp.Web/Controllers/DietController.cs
@@ -3,123 +3,199 @@ using FitnessApp.Data.Models;
 using FitnessApp.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace FitnessApp.Web.Controllers
 {
-    public class DietController : Controller
-    {
-        private readonly ILogger<RecipeController> logger;
-        private readonly UserManager<ApplicationUser> user;
-        private readonly FitnessDBContext context;
+	public class DietController : Controller
+	{
+		private readonly ILogger<RecipeController> logger;
+		private readonly UserManager<ApplicationUser> user;
+		private readonly FitnessDBContext context;
 
-        public DietController(ILogger<RecipeController> _logger, UserManager<ApplicationUser> _user, FitnessDBContext _context)
-        {
-            logger = _logger;
-            user = _user;
-            context = _context;
-        }
+		public DietController(ILogger<RecipeController> _logger, UserManager<ApplicationUser> _user, FitnessDBContext _context)
+		{
+			logger = _logger;
+			user = _user;
+			context = _context;
+		}
 
-        public async Task<IActionResult> Index()
-        {
-            var diets = context.Diets.ToList();
+		public async Task<IActionResult> Index()
+		{
+			var diets = context.Diets.ToList();
 
-            var dietViewModels = diets.Select(diet => new DietIndexView
-            {
-                DietId = diet.Id,
-                Name = diet.Name,
-                Description = diet.Description,
-                ImageUrl = diet.ImageUrl,
-                Calories = diet.Calories,
-                Protein = diet.Protein,
-                Carbohydrates = diet.Carbohydrates,
-                Fats = diet.Fats
-            }).ToList();
+			var dietViewModels = diets.Select(diet => new DietIndexView
+			{
+				DietId = diet.Id,
+				Name = diet.Name,
+				Description = diet.Description,
+				ImageUrl = diet.ImageUrl,
+				Calories = diet.Calories,
+				Protein = diet.Protein,
+				Carbohydrates = diet.Carbohydrates,
+				Fats = diet.Fats
+			}).ToList();
 
-            return View(dietViewModels);
-        }
+			return View(dietViewModels);
+		}
 
-        [HttpGet]
-        public async Task<IActionResult> Details(Guid id)
-        {
-            var recipes = context.Recipes.Where(r => r.DietsRecipes.Any(d => d.DietId == id)).ToList();
+		[HttpGet]
+		public async Task<IActionResult> Details(Guid id)
+		{
+			var recipes = context.Recipes.Where(r => r.DietsRecipes.Any(d => d.DietId == id)).ToList();
 
-            List<DietDetailsView> dietViewModels = recipes.Select(recipe => new DietDetailsView
-            {
-                DietId = id,
-                RecipeId = recipe.Id,
-                Name = recipe.Name,
-                ImageUrl = recipe.ImageUrl,
-                Calories = recipe.Calories,
-                Protein = recipe.Protein,
-                Carbohydrates = recipe.Carbohydrates,
-                Fats = recipe.Fats
-            }).ToList();
+			List<DietDetailsView> dietViewModels = recipes.Select(recipe => new DietDetailsView
+			{
+				DietId = id,
+				RecipeId = recipe.Id,
+				Name = recipe.Name,
+				ImageUrl = recipe.ImageUrl,
+				Calories = recipe.Calories,
+				Protein = recipe.Protein,
+				Carbohydrates = recipe.Carbohydrates,
+				Fats = recipe.Fats
+			}).ToList();
 
-            return View(dietViewModels);
-        }
+			return View(dietViewModels);
+		}
 
-        [HttpGet]
-        public IActionResult DetailsInDiet(Guid recipeId, Guid dietId)
-        {
-            var recipe = context.Recipes
-                .Where(r => r.Id == recipeId)
-                .FirstOrDefault();
+		[HttpGet]
+		public IActionResult DetailsInDiet(Guid recipeId, Guid dietId)
+		{
+			var recipe = context.Recipes
+				.Where(r => r.Id == recipeId)
+				.FirstOrDefault();
 
-            if (recipe == null)
-            {
-                return NotFound();
-            }
+			if (recipe == null)
+			{
+				return NotFound();
+			}
 
-            var viewModel = new RecipeDetailsInDiet
-            {
-                DietId = dietId,
-                RecipeId = recipeId,
-                Name = recipe.Name,
-                Calories = recipe.Calories,
-                Protein = recipe.Protein,
-                Carbohydrates = recipe.Carbohydrates,
-                Fats = recipe.Fats,
-                ImageUrl = recipe.ImageUrl,
-                Ingredients = recipe.Ingredients,
-                Preparation = recipe.Preparation
-            };
+			var viewModel = new RecipeDetailsInDiet
+			{
+				DietId = dietId,
+				RecipeId = recipeId,
+				Name = recipe.Name,
+				Calories = recipe.Calories,
+				Protein = recipe.Protein,
+				Carbohydrates = recipe.Carbohydrates,
+				Fats = recipe.Fats,
+				ImageUrl = recipe.ImageUrl,
+				Ingredients = recipe.Ingredients,
+				Preparation = recipe.Preparation
+			};
 
-            return View(viewModel);
-        }
+			return View(viewModel);
+		}
 
-        public IActionResult AddToDiet()
-        {
-            throw new NotImplementedException();
-        }
+		public IActionResult RemoveFromDiet(Guid dietId, Guid recipeId)
+		{
+			var toRemove = context.DietsRecipes
+				.Where(x => x.RecipeId == recipeId && x.DietId == dietId)
+				.FirstOrDefault();
 
-        public IActionResult RemoveFromDiet(Guid dietId, Guid recipeId)
-        {
-            var toRemove = context.DietsRecipes
-                .Where(x => x.RecipeId == recipeId && x.DietId == dietId)
-                .FirstOrDefault();
+			if (toRemove != null)
+			{
+				context.DietsRecipes.Remove(toRemove);
 
-            if (toRemove != null)
-            {
-                context.DietsRecipes.Remove(toRemove);
+				var diet = context.Diets
+					.Include(d => d.DietsRecipes)
+					.ThenInclude(dr => dr.Recipe)
+					.FirstOrDefault(d => d.Id == dietId);
 
-                var diet = context.Diets
-                    .Include(d => d.DietsRecipes)
-                    .ThenInclude(dr => dr.Recipe)
-                    .FirstOrDefault(d => d.Id == dietId);
+				if (diet != null)
+				{
+					diet.Calories = diet.DietsRecipes.Sum(df => df.Recipe.Calories);
+					diet.Protein = diet.DietsRecipes.Sum(df => df.Recipe.Protein);
+					diet.Carbohydrates = diet.DietsRecipes.Sum(df => df.Recipe.Carbohydrates);
+					diet.Fats = diet.DietsRecipes.Sum(df => df.Recipe.Fats);
+				}
 
-                if (diet != null)
-                {
-                    diet.Calories = diet.DietsRecipes.Sum(df => df.Recipe.Calories);
-                    diet.Protein = diet.DietsRecipes.Sum(df => df.Recipe.Protein);
-                    diet.Carbohydrates = diet.DietsRecipes.Sum(df => df.Recipe.Carbohydrates);
-                    diet.Fats = diet.DietsRecipes.Sum(df => df.Recipe.Fats);
-                }
+				context.SaveChanges();
+			}
 
-                context.SaveChanges();
-            }
+			return RedirectToAction("Details", new { id = dietId });
+		}
 
-            return RedirectToAction("Details", new { id = dietId });
-        }
-    }
+
+
+
+		[HttpGet]
+		public IActionResult AddToDiet(Guid recipeId)
+		{
+			List<SelectListItem> diets = context.Diets
+				.Select(d => new SelectListItem
+				{
+					Value = d.Id.ToString(),
+					Text = d.Name
+				})
+				.ToList();
+
+			var viewModel = new AddRecipeToDietViewModel
+			{
+				RecipeId = recipeId,
+				Diets = diets
+			};
+
+			return View(viewModel);
+		}
+
+		[HttpPost]
+		public IActionResult AddToDiet(AddRecipeToDietViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				var dietRecipe = new DietRecipe
+				{
+					DietId = model.SelectedDietId,
+					RecipeId = model.RecipeId
+				};
+				;
+				context.DietsRecipes.Add(dietRecipe);
+
+				context.SaveChanges();
+
+				var diet = context.Diets
+					.Include(d => d.DietsRecipes)
+					.ThenInclude(dr => dr.Recipe)
+					.FirstOrDefault(d => d.Id == model.SelectedDietId);
+
+				if (diet != null)
+				{
+					diet.Calories = diet.DietsRecipes
+						.Where(df => df.Recipe != null)
+						.Sum(df => df.Recipe.Calories);
+
+					diet.Protein = diet.DietsRecipes
+						.Where(df => df.Recipe != null)
+						.Sum(df => df.Recipe.Protein);
+
+					diet.Carbohydrates = diet.DietsRecipes
+						.Where(df => df.Recipe != null)
+						.Sum(df => df.Recipe.Carbohydrates);
+
+					diet.Fats = diet.DietsRecipes
+						.Where(df => df.Recipe != null)
+						.Sum(df => df.Recipe.Fats);
+				}
+
+				context.SaveChanges();
+
+				context.SaveChanges();
+
+				return RedirectToAction("Details", new { id = model.SelectedDietId });
+			}
+
+			model.Diets = context.Diets
+				.Select(d => new SelectListItem
+				{
+					Value = d.Id.ToString(),
+					Text = d.Name
+				})
+				.ToList();
+
+			return View(model);
+		}
+	}
 }
