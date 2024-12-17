1c417366467ad2d7a2ceab28488845f58476850a
TotevM <mtotev53@gmail.com>
2024-12-17 22:46:02 +0200

    Removed Unused Constants

--- FitnessApp.Common/EntityValidationConstants.cs
+++ FitnessApp.Common/EntityValidationConstants.cs
@@ -6,24 +6,12 @@
         {
             public const int UsernameMinLength = 3;
             public const int UsernameMaxLength = 32;
-
-            public const int PasswordMinLength = 6;
-            public const int PasswordMaxLength = 32;
-
-            public const int FirstNameMinLength = 1;
-            public const int FirstNameMaxLength = 64;
-
-            public const int LastNameMinLength = 1;
-            public const int LastNameMaxLength = 64;
         }
 
         public static class DietValidation
         {
             public const int DietNameMinLength = 3;
             public const int DietNameMaxLength = 32;
-
-            public const int DescriptionMinLength = 50;
-            public const int DescriptionMaxLength = 300;
         }
 
         public static class ExerciseValidation
@@ -36,9 +24,6 @@
         {
             public const int WeightMax = 300;
             public const int WeightMin = 25;
-
-            public const int HeightMin = 100;
-            public const int HeightMax = 256;
         }
 
         public static class RecipeValidation
@@ -56,17 +41,12 @@
             public const int ProteinMax = 500;
             public const int CarbsMax = 500;
             public const int FatsMax = 500;
-
-            public const string DateFormat = "dd/mm/YYYY";
         }
 
         public static class WorkoutValidation
         {
             public const int WorkoutNameMinLength = 3;
             public const int WorkoutNameMaxLength = 32;
-
-            public const string WorkoutMinPrice = "3";
-            public const string WorkoutMaxPrice = "512";
         }
     }
 }
